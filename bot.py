26fd1bc4b6d36d5ed087381d3eb8fb417d4d0cc9
DebXD <debiprasadxd@protonmail.com>
2023-05-20 19:04:27 +0530

    Revert "Rewritten  python -> node.js"
    This reverts commit 05c4d9dc2267de06e1a11bd03785d89e10aa3106.

---
+++ bot.py
@@ -0,0 +1,60 @@
+from decouple import config
+
+import telebot
+
+import os
+
+from scraper import getArticleContent, tts
+from url_validator import isValidURL
+
+BOT_TOKEN = config("BOT_TOKEN")
+if BOT_TOKEN is None:
+    print("Enter Your Bot Token in .env file.")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "I am your article reader bot.")
+
+#Handle '/tts'
+@bot.message_handler(commands=['tts'])
+def textToSpeech(message):
+    url = message.text
+    modified_url = isValidURL(url)
+    
+    if modified_url == False:
+        bot.reply_to(message, "Provide a website URL.")
+        
+    else:
+        link = modified_url[0]
+        print(link)
+        bot.reply_to(message, "Ok, Processing...it might take sometime")
+        wholeText = getArticleContent(link)
+        if wholeText is not None:
+            title, content = wholeText
+            print("scraping complete")
+            result = tts(title, content)
+        
+            if result == True:
+
+                chat_id = message.chat.id
+                
+
+                audio = open("audio.mp3", 'rb')
+                #bot.send_audio(chat_id, audio)
+                bot.send_audio(chat_id, audio,  performer='Article Reader', title=title)
+                print("Sent successfully")
+                audio.close()
+                if True:
+                    os.remove("audio.mp3")
+            else:
+                bot.reply_to(message, "Sorry, failure converting text to audio")
+            
+        else:
+            bot.reply_to(message, "Sorry, Can't Process this Article")
+
+
+bot.infinity_polling()
--- utils/url_validator.js
+++
@@ -1,8 +0,0 @@
-isValidURL = function(url){
-    
-    const regex = new RegExp('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+');
-    return regex.test(url) // returns boolean 
-
-}
-
-module.exports= isValidURL;
